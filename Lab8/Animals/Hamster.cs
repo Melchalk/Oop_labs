fa474a5178af557b5f86d39a7cb8f9239f85e73a
Marina Mel <melm2004@mail.ru>
2023-11-24 00:31:36 +0300

    done lab8

---
+++ Lab8/Animals/Hamster.cs
@@ -0,0 +1,28 @@
+﻿namespace oop8.Animals;
+
+internal class Hamster : Animal
+{
+    public override string Name { get; set; } = "Hamster";
+    public override string Voice { get; set; } = "Snorting"; //Фырчание
+    public string Dainty { get; set; } = "Seeds";
+
+    public override string Move()
+    {
+        return $"{Name} is running";
+    }
+
+    public string HaveFun()
+    {
+        return $"{Name} is running in the wheel";
+    }
+
+    public override string Say()
+    {
+        return $"{Name} is {Voice.ToLower()}";
+    }
+
+    public string GetDainty()
+    {
+        return $"{Name} got {Dainty.ToLower()}";
+    }
+}
--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oop8.Animals;
+using System.Text;
+
+Hamster hamster = new();
+Rooster rooster = new();
+Turtle turtle = new();
+
+Console.WriteLine(GetInfoAnimal(hamster));
+Console.WriteLine();
+
+Console.WriteLine(GetInfoAnimal(rooster));
+Console.WriteLine();
+
+Console.WriteLine(GetInfoAnimal(turtle));
+Console.WriteLine();
+
+string GetInfoAnimal(Animal animal)
+{
+    StringBuilder infoOfAnimal = new();
+
+    infoOfAnimal.Append($"---Name of animal - {animal.Name}---\n");
+
+    infoOfAnimal.Append($"Voice of animal: {animal.Say()}\n");
+
+    infoOfAnimal.Append($"Animal movement: {animal.Move()}\n");
+
+    if (animal is Hamster hamster)
+    {
+        infoOfAnimal.Append(hamster.GetDainty());
+    }
+    else if (animal is Rooster rooster)
+    {
+        infoOfAnimal.Append(rooster.BecomeAlpha());
+
+        infoOfAnimal.Append('\n');
+
+        infoOfAnimal.Append(rooster.StopBeingAlpha());
+    }
+    else if (animal is Turtle turtle)
+    {
+        infoOfAnimal.Append(turtle.Hide());
+    }
+
+    return infoOfAnimal.ToString();
+}
