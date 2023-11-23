c46e73bd4e2cb3e5106bef65e6eea2bfed9bb16b
Marina Mel <melm2004@mail.ru>
2023-11-24 00:40:35 +0300

    done lab9

---
+++ Lab9/Notifications/EmailNotification.cs
@@ -0,0 +1,25 @@
+﻿namespace oop9.Notifications;
+
+internal class EmailNotification : Notification
+{
+    public string Mail { get; set; }
+    public string MailDomain { get; set; }
+
+    public EmailNotification(string senderNickname, string mail, string content) : base(senderNickname, content)
+    {
+        if (mail.Contains('@'))
+        {
+            Mail = mail;
+            MailDomain = mail[(mail.IndexOf('@') + 1)..];
+        }
+        else
+        {
+            throw new Exception("Invalid email address");
+        }
+    }
+
+    public override string GetInfo()
+    {
+        return $"{Mail}: {Content}";
+    }
+}
