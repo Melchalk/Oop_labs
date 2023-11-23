d6ab5126f4c6e97d9c67971900d1f8b4376a0c9d
Marina Melnikova <108426963+Melchalk@users.noreply.github.com>
2023-11-23 23:16:48 +0300

    done lab3
    * moved to the old lab3
    
    * done lab3

---
+++ Lab3/StudentsRepository.cs
@@ -0,0 +1,49 @@
+﻿using Lab3;
+using System.Text;
+
+namespace DataAccess;
+
+static internal class StudentsRepository
+{
+    static readonly string _file = Directory.GetCurrentDirectory() + "/Students.txt";
+
+    public static void Save(List<Student> students)
+    {
+        StringBuilder stringBuilder = new();
+
+        for (int indexStudent = 0; indexStudent < students.Count; indexStudent++)
+        {
+            Student student = students[indexStudent];
+
+            stringBuilder.Append($"{student.Name}\t{student.LastName}\t{student.Age}\t{student.AverageScore}");
+
+            if (indexStudent != students.Count - 1)
+            {
+                stringBuilder.Append('\n');
+            }
+        }
+
+        File.WriteAllText(_file, stringBuilder.ToString());
+    }
+
+    public static List<Student> Read()
+    {
+        List<Student> students = new();
+        var listOfStudent = File.ReadAllText(_file).Split('\n');
+
+
+        for (int i = 0; i < listOfStudent.Length; i += 1)
+        {
+            var infoStudent = listOfStudent[i].Split('\t');
+
+            Student student = new(infoStudent[0], infoStudent[1], int.Parse(infoStudent[2]))
+            {
+                AverageScore = double.Parse(infoStudent[3])
+            };
+
+            students.Add(student);
+        }
+
+        return students;
+    }
+}
---
+++ Lab3/University.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab3;
+
+internal class University
+{
+    readonly List<Student> students = new();
+
+    public void AddStudent(Student newStudent)
+    {
+        students.Add(newStudent); 
+    }
+    public bool RemoveStudent(Student newStudent)
+    {
+        return students.Remove(newStudent);
+    }
+    public int FindStudent(Student student)
+    {
+        return students.IndexOf(student);
+    }
+
+    public List<Student> GetStudents()
+    {
+        return students;
+    }
+}
