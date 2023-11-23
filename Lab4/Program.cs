62c261c25fe464e3d187d55bd93468c2712f32ab
Marina Mel <melm2004@mail.ru>
2023-11-23 23:37:16 +0300

    done lab4

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oop4;
+
+User user = new()
+{
+    Age = 25,
+    Name = "Вася"
+};
+
+Console.WriteLine(user.GetInfo());
+
+//Возраст не изменится
+user.Age = -5;
+Console.WriteLine(user.GetInfo());
+
+//Вывод без возраста
+Console.WriteLine(user.GetInfo(false));
