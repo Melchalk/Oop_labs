7d7cee09c44cb5530d46ced2c053f89c2fe0fc5e
Marina Mel <melm2004@mail.ru>
2023-11-24 00:04:54 +0300

    add file from old repo + update

---
+++ Lab6/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace oop6;
+
+internal class Product
+{
+    //подавление серьезности
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+    public string Title { get; set; }
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+    public double Price { get; set; }
+    public int Discount { get; set; }
+    public string? Manufacturer { get; set; }
+    public int? Quantity { get; set; }
+    public double? Code { get; set; }
+
+    readonly static string _isNull = "Не задано";
+
+    public string GetInfo()
+    {
+        string _info = $"Название продукта - {Title}\n" +
+            $"Стоимость - {Price}\n" +
+            $"Скидка - {Discount}\n" +
+            $"Производитель - {Manufacturer ?? _isNull}\n";
+
+        string _countInfo = "Количество - {0}\n";
+        string _codeInfo = $"Код продукта - {_isNull}\n";
+
+        _info += Quantity is null
+            ? string.Format(_countInfo, _isNull)
+            : string.Format(_countInfo, Quantity);
+
+        _info += Code is null
+            ? string.Format(_codeInfo, _isNull)
+            : string.Format(_codeInfo, Code);
+
+        return _info;
+    }
+
+    public static void IfNull(Product product)
+    {
+        product.Manufacturer ??= _isNull;
+        product.Quantity ??= 0;
+        product.Code ??= 0;
+    }
+
+    public static string GetManufacturer(Product? product)
+    {
+        return product?.Manufacturer ?? _isNull;
+    }
+}
--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using oop6;
+
+Product product = new()
+{
+    Title = "Молоко",
+    Quantity = 140,
+};
+
+Product product1 = new()
+{
+    Manufacturer = "Домик в деревне",
+    Price = 63,
+};
+
+//Не nullable будут проиндексированы стандартными значениями, для Title это null
+Console.WriteLine(product.GetInfo());
+Console.WriteLine(product1.GetInfo());
+
+// ! - Non-Nullable, без него предупреждение о присваении null
+product1.Title = null!;
+
+//С значимыми типами ! не помогает
+//product.Discount = null!;
+
+//Без предпреждения, так как названия не должно иметь значение null
+int count = product.Title.Length;
+//int count2 = product1.Title.Length; //Выдаст исключение
+
+Product? product2 = null;
+
+Console.WriteLine(Product.GetManufacturer(product));
+Console.WriteLine(Product.GetManufacturer(product1));
+Console.WriteLine(Product.GetManufacturer(product2));
+Console.WriteLine();
+
+Product.IfNull(product);
+Console.WriteLine(product.GetInfo());
